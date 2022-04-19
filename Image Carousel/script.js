92b11c29e72762a067e2b94050632d39a7232413
Mooka <47282413+mooka007@users.noreply.github.com>
2022-04-19 03:52:08 +0000

    carousel functionality
    carousel functionality

---
+++ Image Carousel/script.js
@@ -0,0 +1,33 @@
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+
+const img = document.querySelectorAll('#imgs img')
+
+//b ta3tina whd nodeList() dyal img li kaunin f imgs
+// console.log(img)
+let idx = 0;
+// function run (){
+//     idx++;
+//     changeImage();
+// }
+
+function changeImage(){
+    if(idx > img.length - 1 ){
+        idx = 0;
+    }else if(idx < 0 ) {
+        idx = img.length - 1;
+    }
+    imgs.style.transform = `translateX(${-idx * 400}px)` 
+}
+
+rightBtn.addEventListener('click', () =>{
+    idx++;
+    changeImage();
+})
+
+leftBtn.addEventListener('click', () =>{
+    idx--;
+    changeImage();
+})
